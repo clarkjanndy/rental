01afd15c57420a29a6d57b36ad5cd81059decfd0
unknown <cjcasol22@gmail.com>
2022-12-07 22:33:53 +0800

    Add test.py script

--- main/tests.py
+++ main/tests.py
@@ -1,5 +1,85 @@
-from django.test import TestCase
+from django.urls import reverse
 from rest_framework.test import APITestCase
+from main.models import Rental, Reservation
 
 # Create your tests here.
-# class AuthTest(APITestCase):
+class Test(APITestCase):
+
+    #set up test data here
+    def setUp(self):
+        rental1=Rental.objects.create(name='rental-1')
+        rental2=Rental.objects.create(name='rental-2')
+
+        Reservation.objects.create(rental=rental1, check_in='2022-01-01', check_out='2022-01-13')
+        Reservation.objects.create(rental=rental1, check_in='2022-01-20', check_out='2022-02-10')
+        Reservation.objects.create(rental=rental1, check_in='2022-02-20', check_out='2022-03-10')
+        Reservation.objects.create(rental=rental2, check_in='2022-01-02', check_out='2022-01-20')
+        Reservation.objects.create(rental=rental2, check_in='2022-01-11', check_out='2022-01-20')
+
+
+    def test_1(self):
+        response = self.client.get('/reservations/1', format='json')
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data['rental'], 'rental-1')
+        self.assertEqual(response.data['id'], 'Res-1')
+        self.assertEqual(response.data['check_in'], '2022-01-01')
+        self.assertEqual(response.data['check_out'], '2022-01-13')
+        self.assertEqual(response.data['previous_reservation'], '-')
+    
+    def test_2(self):
+        response = self.client.get('/reservations/2', format='json')
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data['rental'], 'rental-1')
+        self.assertEqual(response.data['id'], 'Res-2')
+        self.assertEqual(response.data['check_in'], '2022-01-20')
+        self.assertEqual(response.data['check_out'], '2022-02-10')
+        self.assertEqual(response.data['previous_reservation'], 'Res-1')
+    
+    def test_3(self):
+        response = self.client.get('/reservations/3', format='json')
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data['rental'], 'rental-1')
+        self.assertEqual(response.data['id'], 'Res-3')
+        self.assertEqual(response.data['check_in'], '2022-02-20')
+        self.assertEqual(response.data['check_out'], '2022-03-10')
+        self.assertEqual(response.data['previous_reservation'], 'Res-2')
+    
+    def test_4(self):
+        response = self.client.get('/reservations/4', format='json')
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data['rental'], 'rental-2')
+        self.assertEqual(response.data['id'], 'Res-4')
+        self.assertEqual(response.data['check_in'], '2022-01-02')
+        self.assertEqual(response.data['check_out'], '2022-01-20')
+        self.assertEqual(response.data['previous_reservation'], '-')
+    
+    def test_5(self):
+        response = self.client.get('/reservations/5', format='json')
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data['rental'], 'rental-2')
+        self.assertEqual(response.data['id'], 'Res-5')
+        self.assertEqual(response.data['check_in'], '2022-01-11')
+        self.assertEqual(response.data['check_out'], '2022-01-20')
+        self.assertEqual(response.data['previous_reservation'], 'Res-4')
+
+    def test_6(self):
+        '''
+        Test to check if the error message is displayed.
+        '''
+        data = {"rental": 1, "check_in": '2022-01-20', "check_out": '2022-01-11'}
+        response = self.client.post('/reservations', data, format='json')
+
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.data['date error'], ["Check-In date must not be a date before Check-Out date."])
+            
+        
+
+    
+    
+        
+        
